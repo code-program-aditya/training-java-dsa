21284e214a06de617cda0245869c68972fcb5641
adityaraj <adityarajdps9e@outlook.com>
2026-06-09 10:46:50 +0530

    add

---
+++ code23.java
@@ -0,0 +1,8 @@
+public class code23 {
+    public static void main(String[] args) {
+        String Name = "CodeChef";
+        Name = "od";
+        System.out.println(Name);
+    }
+    
+}
---
+++ code24.java
@@ -0,0 +1,7 @@
+public class code24 {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        s1 = s1.concat(" World");
+        System.out.println(s1);
+    }
+}
